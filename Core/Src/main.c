a5da9ba86d958fea5767174c83edb47446fede4e
arek <areklib@poczta.onet.pl>
2024-03-04 12:05:30 +0100

    Upper text is displayed, lower a bit cutted. But everything is off displayed 3 times, than after screen is cleared. And green led start to pulse.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,6 +22,12 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "stm32l0xx_nucleo.h"
+
+#include "f1206.h"
+#include "f1608.h"
+#include "f3216.h"
+#include "disp_lib.h"
+
 #include "sh1106_drv.h"
 /* USER CODE END Includes */
 
@@ -48,8 +54,14 @@ SPI_HandleTypeDef hspi1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
 uint8_t txBuffer[BUFFERSIZE];
+
+//example from ui.c init()
+static const char *rpm_default = "9999";
+static const char *temperat_default = "999";
+static const char *volt_default = "99.99";
+static const char *mth_default = "9999";
+
 
 /* USER CODE END PV */
 
@@ -111,12 +123,12 @@ int main(void)
 
 	// Set default values
   // UI - ok ready.
-/*
+
 	DISP_drawString(2, 8, rpm_default, DISP_FONT1612, DISP_FONT_POSITIVE);
 	DISP_drawString(18, 40, temperat_default, DISP_FONT1612, DISP_FONT_POSITIVE);
 	DISP_drawString(82, 8, volt_default, DISP_FONT1608, DISP_FONT_POSITIVE);
 	DISP_drawString(90, 40, mth_default, DISP_FONT1608, DISP_FONT_POSITIVE);
-*/
+
 	SH1106_updateScreen();
 	//_delay_ms(4000);  // 4s
 	HAL_Delay(4000);
--- Debug/Core/Src/subdir.mk
+++ Debug/Core/Src/subdir.mk
@@ -5,6 +5,11 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../Core/Src/disp_lib.c \
+../Core/Src/f1206.c \
+../Core/Src/f1608.c \
+../Core/Src/f1612.c \
+../Core/Src/f3216.c \
 ../Core/Src/main.c \
 ../Core/Src/sh1106_drv.c \
 ../Core/Src/stm32l0xx_hal_msp.c \
@@ -15,6 +20,11 @@ C_SRCS += \
 ../Core/Src/system_stm32l0xx.c 
 
 OBJS += \
+./Core/Src/disp_lib.o \
+./Core/Src/f1206.o \
+./Core/Src/f1608.o \
+./Core/Src/f1612.o \
+./Core/Src/f3216.o \
 ./Core/Src/main.o \
 ./Core/Src/sh1106_drv.o \
 ./Core/Src/stm32l0xx_hal_msp.o \
@@ -25,6 +35,11 @@ OBJS += \
 ./Core/Src/system_stm32l0xx.o 
 
 C_DEPS += \
+./Core/Src/disp_lib.d \
+./Core/Src/f1206.d \
+./Core/Src/f1608.d \
+./Core/Src/f1612.d \
+./Core/Src/f3216.d \
 ./Core/Src/main.d \
 ./Core/Src/sh1106_drv.d \
 ./Core/Src/stm32l0xx_hal_msp.d \
@@ -42,7 +57,7 @@ Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
 clean: clean-Core-2f-Src
 
 clean-Core-2f-Src:
-	-$(RM) ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/sh1106_drv.cyclo ./Core/Src/sh1106_drv.d ./Core/Src/sh1106_drv.o ./Core/Src/sh1106_drv.su ./Core/Src/stm32l0xx_hal_msp.cyclo ./Core/Src/stm32l0xx_hal_msp.d ./Core/Src/stm32l0xx_hal_msp.o ./Core/Src/stm32l0xx_hal_msp.su ./Core/Src/stm32l0xx_it.cyclo ./Core/Src/stm32l0xx_it.d ./Core/Src/stm32l0xx_it.o ./Core/Src/stm32l0xx_it.su ./Core/Src/stm32l0xx_nucleo.cyclo ./Core/Src/stm32l0xx_nucleo.d ./Core/Src/stm32l0xx_nucleo.o ./Core/Src/stm32l0xx_nucleo.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32l0xx.cyclo ./Core/Src/system_stm32l0xx.d ./Core/Src/system_stm32l0xx.o ./Core/Src/system_stm32l0xx.su
+	-$(RM) ./Core/Src/disp_lib.cyclo ./Core/Src/disp_lib.d ./Core/Src/disp_lib.o ./Core/Src/disp_lib.su ./Core/Src/f1206.cyclo ./Core/Src/f1206.d ./Core/Src/f1206.o ./Core/Src/f1206.su ./Core/Src/f1608.cyclo ./Core/Src/f1608.d ./Core/Src/f1608.o ./Core/Src/f1608.su ./Core/Src/f1612.cyclo ./Core/Src/f1612.d ./Core/Src/f1612.o ./Core/Src/f1612.su ./Core/Src/f3216.cyclo ./Core/Src/f3216.d ./Core/Src/f3216.o ./Core/Src/f3216.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/sh1106_drv.cyclo ./Core/Src/sh1106_drv.d ./Core/Src/sh1106_drv.o ./Core/Src/sh1106_drv.su ./Core/Src/stm32l0xx_hal_msp.cyclo ./Core/Src/stm32l0xx_hal_msp.d ./Core/Src/stm32l0xx_hal_msp.o ./Core/Src/stm32l0xx_hal_msp.su ./Core/Src/stm32l0xx_it.cyclo ./Core/Src/stm32l0xx_it.d ./Core/Src/stm32l0xx_it.o ./Core/Src/stm32l0xx_it.su ./Core/Src/stm32l0xx_nucleo.cyclo ./Core/Src/stm32l0xx_nucleo.d ./Core/Src/stm32l0xx_nucleo.o ./Core/Src/stm32l0xx_nucleo.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32l0xx.cyclo ./Core/Src/system_stm32l0xx.d ./Core/Src/system_stm32l0xx.o ./Core/Src/system_stm32l0xx.su
 
 .PHONY: clean-Core-2f-Src
 
